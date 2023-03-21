9c0dc53d02fe9eec558cb073acf19a564543a064
Nihil02 <samuelalva02@gmail.com>
2023-03-21 08:57:09 -0600

    array test

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,10 +1,25 @@
 import { FaPlus, FaPrint, FaTrash, FaPen } from "react-icons/fa";
 
+const cards = [
+  {
+    nombre: "Ab",
+  },
+  {
+    nombre: "Ba",
+  },
+];
+
 function Content() {
+  function renderCards() {
+    return cards.map((card) => {
+      return <Card name={card.nombre}/>;
+    });
+  }
+
   return (
     <>
       <AddCardButton />
-      <Card name="Maria Agustina Concepcion de la Torre Castro y Castro" />
+      {renderCards()}
     </>
   );
 }
@@ -32,13 +47,22 @@ const Card = ({ name = "" }) => {
         </p>
       </div>
       <div className="flex flex-wrap absolute items-center gap-y-2 gap-x-4 right-4">
-        <button className="card-button bg-yellow-400 border-yellow-400" onClick={cardPrint}>
+        <button
+          className="card-button bg-yellow-400 border-yellow-400"
+          onClick={cardPrint}
+        >
           <FaPrint size={16} color="white" />
         </button>
-        <button className="card-button bg-green-400 border-green-400" onClick={cardUpdate}>
+        <button
+          className="card-button bg-green-400 border-green-400"
+          onClick={cardUpdate}
+        >
           <FaPen size={16} color="white" />
         </button>
-        <button className="card-button bg-red-400 border-red-400" onClick={cardDelete}>
+        <button
+          className="card-button bg-red-400 border-red-400"
+          onClick={cardDelete}
+        >
           <FaTrash size={16} color="white" />
         </button>
       </div>
