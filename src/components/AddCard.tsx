d1d82da3a208f249297230677659d275eba992a5
Nihil02 <samuelalva02@gmail.com>
2023-03-27 18:00:52 -0600

    Basic form

--- src/components/AddCard.tsx
+++ src/components/AddCard.tsx
@@ -3,6 +3,15 @@ import { Fragment, useState } from "react";
 import { FaPlus } from "react-icons/fa";
 
 function AddCliente() {
+  let [curp, setCURP] = useState("");
+  let [nombre, setNombre] = useState("");
+  let [apellido1, setApellido1] = useState("");
+  let [apellido2, setApellido2] = useState("");
+  let [fecha, setFecha] = useState("");
+  let [estado, setEstado] = useState("");
+  let [municipio, setMunicipio] = useState("");
+  let [locacion, setLocacion] = useState("");
+
   let [isOpen, setIsOpen] = useState(false);
   function closeModal() {
     setIsOpen(false);
@@ -50,9 +59,152 @@ function AddCliente() {
               >
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <form className="m-4" onSubmit={addCard}>
-                    <button type="submit" className="btn-primary">
-                      Agregar
-                    </button>
+                    <div className="mb-6">
+                      <label htmlFor="">CURP</label>
+                      <input
+                        type="text"
+                        id=""
+                        name=""
+                        className="text-input"
+                        placeholder="CURP"
+                        onChange={(e) => setCURP(e.target.value)}
+                        pattern="[A-Za-z]{4}[\d]{6}[H|M][A-Za-z]{5}[A-Za-z\d]{2}$"
+                        required
+                      />
+                    </div>
+                    <div className="mb-6">
+                      <label htmlFor="">Nombre</label>
+                      <input
+                        type="text"
+                        id=""
+                        name=""
+                        maxLength={50}
+                        className="text-input"
+                        placeholder="Nombre"
+                        onChange={(e) => setNombre(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="mb-6">
+                      <label htmlFor="">Primer Apellido</label>
+                      <input
+                        type="text"
+                        id=""
+                        name=""
+                        maxLength={50}
+                        className="text-input"
+                        placeholder="Primer Apellido"
+                        onChange={(e) => setApellido1(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="mb-6">
+                      <label htmlFor="">Segundo Apellido</label>
+                      <input
+                        type="text"
+                        id=""
+                        name=""
+                        maxLength={50}
+                        className="text-input"
+                        placeholder="Segundo Apellido"
+                        onChange={(e) => setApellido2(e.target.value)}
+                      />
+                    </div>
+                    <div className="mb-6">
+                      <label htmlFor="">Fecha de Nacimiento</label>
+                      <input
+                        type="date"
+                        id=""
+                        name=""
+                        className="text-input"
+                        required
+                      />
+                    </div>
+                    <div className="mb-6">
+                      <fieldset>
+                        <legend className="text-gray-900 text-sm leading-6">
+                          Sexo
+                        </legend>
+                        <div className="mt-6 space-y-6">
+                          <div className="flex items-center gap-x-3">
+                            <input
+                              id="sexo_h"
+                              name="hombre"
+                              type="radio"
+                              className="h-4 w-4 "
+                            />
+                            <label
+                              htmlFor="sexo_h"
+                              className="block leading-6 text-gray-900 text-sm"
+                            >
+                              Hombre
+                            </label>
+                          </div>
+                          <div className="flex items-center gap-x-3">
+                            <input
+                              id="sexo_m"
+                              name="mujer"
+                              type="radio"
+                              className="h-4 w-4 "
+                            />
+                            <label
+                              htmlFor="sexo_m"
+                              className="block leading-6 text-gray-900 text-sm"
+                            >
+                              Mujer
+                            </label>
+                          </div>
+                        </div>
+                      </fieldset>
+                    </div>
+                    <div className="mb-6">
+                      <label htmlFor="">Estado</label>
+                      <input
+                        type="number"
+                        id=""
+                        name=""
+                        className="text-input"
+                        placeholder="Estado"
+                        onChange={(e) => setEstado(e.target.value)}
+                        min={1}
+                        max={32}
+                      />
+                    </div>
+                    <div className="mb-6">
+                      <label htmlFor="">Municipio</label>
+                      <input
+                        type="number"
+                        id=""
+                        name=""
+                        className="text-input"
+                        placeholder="Municipio"
+                        onChange={(e) => setMunicipio(e.target.value)}
+                        min={1}
+                        max={999}
+                      />
+                    </div>
+                    <div className="mb-6">
+                      <label htmlFor="">Locación</label>
+                      <input
+                        type="number"
+                        id=""
+                        name=""
+                        className="text-input"
+                        placeholder="Locación"
+                        onChange={(e) => setLocacion(e.target.value)}
+                        min={1}
+                        max={9999}
+                      />
+                    </div>
+
+                    <div className="flex items-center justify-center gap-x-6 mt-4">
+                      <button type="submit" className="btn-primary">
+                        Agregar
+                      </button>
+                      <button className="btn-danger" onClick={closeModal}>
+                        Cancelar
+                      </button>
+                    </div>
                   </form>
                 </Dialog.Panel>
               </Transition.Child>
