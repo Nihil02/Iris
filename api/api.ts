434bc3abca66950c3e9f7df4e8108e05867f3f1d
Raynou <rafaelgame2012@gmail.com>
2023-03-29 11:35:53 -0600

    getAllCostumers function working

--- api/api.ts
+++ api/api.ts
@@ -1,14 +1,16 @@
+interface Data {
+    dataValues: Object
+}
 class API {
     /**
-     * 
-     * @returns a JSON array of costumers.
+     * @returns Promise with JSON array costumers
      */
-    static async getAllSuppliers() {
-        const res = await window.modelAPI.getAllProveedores();
+    static async getAllSuppliers():Promise<Object[]> {
+        const res:Array<Data> = await window.modelAPI.getAllProveedores();
         const costumers = res.map(costumer => {
             return costumer.dataValues;
         });
-        return res;
+        return costumers;
     }
 }
 
