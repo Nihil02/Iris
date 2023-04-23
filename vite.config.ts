510790d3a9db7f8231192ed813b9b7ce3adebf50
Nihil02 <samuelalva02@gmail.com>
2023-04-22 18:20:55 -0600

    gitignore and viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   base: process.env.IS_DEV !== 'true' ? './' : '/',
   build: {
     target: "ESnext",
-    outDir: 'electron/build',
+    outDir: 'backend/electron/build',
   },
   plugins: [react()],
 })
