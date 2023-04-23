21e93f4f4cce8133b519d5b92a7451fdedc05b57
Raymonds <rafaelgame2012@gmail.com>
2023-04-22 20:40:19 -0600

    add api for employee

--- backend/api/employee.ts
+++ backend/api/employee.ts
@@ -1,38 +1,69 @@
+class Employee {
+  rfc: string;
+  name: string;
+  firstLastName: string;
+  secondLastName: string;
+  password: string;
 
-interface Data {
-  dataValues: Object;
-}
-
-interface User {
-  rfc: string,
-  name: string,
-  firstLastName: string,
-  secondLastName: string,
-  password: string
+  constructor(
+    rfc: string,
+    name: string,
+    firstLastName: string,
+    secondLastName: string,
+    password: string
+  ) {
+    this.rfc = rfc;
+    this.name = name;
+    this.firstLastName = firstLastName;
+    this.secondLastName = secondLastName;
+    this.password = password;
+  }
 }
 class EmployeeAPI {
   /**
-   * @returns Promise with JSON array suppliers
+   * @returns Promise with JSON array employees
    */
-  static async getAllUsers() {
-    const res: Array<Data> = await window.userAPI.getAllUsers();
+  static async getAllEmployees() {
+    const res: Array<Data> = await window.userAPI.getAllEmployees();
 
-    const users = res.map((user) => {
-      return user.dataValues;
+    const employees = res.map((employee) => {
+      return employee.dataValues;
     });
-    
-    return users;
+
+    return employees;
   }
 
-  static async getUserByRFC(rfc: string) {
-    const res: Data = await window.userAPI.getUserByRFC(rfc);
+  /**
+   *
+   * @param rfc The RFC of the user
+   * @returns Employee
+   */
+  static async getEmployeeByRFC(rfc: string) {
+    const res: Data = await window.userAPI.getEmployeeByRFC(rfc);
+    return res.dataValues;
   }
 
-  static async createUser(user: User) {
-    const res: Boolean = await window.userAPI.createUser(user);
+  /**
+   * Creates a new Employee.
+   * @param Employee
+   * @returns True if the user has been created
+   */
+
+  static async createEmployee(employee: Employee) {
+    const res: Boolean = await window.userAPI.createEmployee(user);
     return res;
   }
 
+  /**
+   * Deletes an employee
+   * @param RFC - The rfc of the employee
+   * returns True if the employee has been deleted
+   */
+
+  static async deleteEmployee(rfc: string) {
+    const res: Boolean = await window.userAPI.deleteEmployee(rfc);
+    return res;
+  }
 }
 
-export default EmployeeAPI;
+export { EmployeeAPI, Employee };
