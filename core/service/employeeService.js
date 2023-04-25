922038b48cee58457cecbb58419819299660803d
Raynou <rafaelgame2012@gmail.com>
2023-04-24 18:06:51 -0600

    advances in employee service

--- core/service/employeeService.js
+++ core/service/employeeService.js
@@ -1,27 +1,53 @@
 const EmployeeRepository = require("../repository/employeeRepository.js");
+const Validator = require("../validation/validator.js");
 
 class EmployeeService {
   static async getAllEmployees() {
     const res = await EmployeeRepository.getAllEmployees();
-    const employees = res.forEach((employee) => {
+    const employees = res.map((employee) => {
       return employee.dataValues;
     });
     return employees;
   }
 
-  static async getEmployeeByRFC(rfc) {
-    const employee = await EmployeeRepository.getEmpleeByRFC(rfc);
-    return employee.dataValues;
+  /**
+   * Finds an employee by his RFC and returns him if exists.
+   * @param RFC - The RFC of the Employee.
+   * @returns An object with the employee data.
+   */
+  static async getEmployeeByRFC(rfc = "") {
+    try {
+      const sanitizedRFC = rfc.trim();
+      if (!Validator.isRFC(sanitizedRFC)) {
+        throw Error("Invalid RFC");
+      }
+      const employee = await EmployeeRepository.getEmpleeByRFC(rfc);
+      return employee[0].dataValues;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
+  /**
+   * Recives an object with the data of the employee and creates him in the database.
+   * @returns True if the user has been created in other case throws an error.
+   */
   static async createEmployee(employee) {
     try {
-      /* Validations goes here */
-      await EmployeeRepository.createEmployee(employee);
-      return true;
-    } catch (e) {
-      console.err(e);
-      return false;
+      const sanitizedEmployee = this.sanitizeEmployee(employee);
+      const validation = this.isValidEmployee(sanitizedEmployee);
+
+      if(typeof validation === 'object') {
+        throw validation;
+      }
+      const findEmployee = await this.getEmployeeByRFC(sanitizedEmployee.rfc);
+      if(findEmployee) {
+        throw new Error("Employee already exists");
+      }
+
+      return await EmployeeRepository.createEmployee(sanitizedEmployee);
+    } catch (error) {
+      return error;
     }
   }
 
@@ -30,8 +56,8 @@ class EmployeeService {
       /* Validations goes here */
       await EmployeeRepository.updateEmployee(employee);
       return false;
-    } catch (e) {
-      console.error(e);
+    } catch (error) {
+      console.error(error);
       return false;
     }
   }
@@ -40,8 +66,8 @@ class EmployeeService {
     try {
       await EmployeeRepository.deleteEmployee(rfc);
       return true;
-    } catch (e) {
-      console.error(e);
+    } catch (error) {
+      console.error(error);
       return false;
     }
   }
@@ -55,6 +81,47 @@ class EmployeeService {
       console.error(e);
       return false;
     }
+  }
+
+  static sanitizeEmployee(employee) {
+    let { rfc, name, firstLastName, secondLastName, password } = employee;
+    rfc = rfc.trim();
+    name = name.trim();
+    firstLastName = firstLastName.trim();
+    secondLastName = secondLastName.trim();
+    password = this.hashPassword(password);
+
+    return {
+      rfc: rfc,
+      name: name,
+      firstLastName: firstLastName,
+      secondLastName: secondLastName,
+      password: password,
+    };
+  }
+
+  static hashPassword(password) {
+    return password;
+  }
+
+  static isValidEmployee(employee) {
+    const { rfc, name, firstLastName, secondLastName, password } = employee;
+    if (!Validator.isName(name)) {
+      return new Error("Invalid name");
+    }
+
+    if (!Validator.isName(firstLastName)) {
+      return new Error("Invalid first last name");
+    }
+
+    if (!Validator.isName(secondLastName)) {
+      return new Error("Invalid second last name");
+    }
+
+    if (!Validator.isRFC(rfc)) {
+      return new Error("Invalid RFC");
+    }
+    return true;
   }
 }
 
